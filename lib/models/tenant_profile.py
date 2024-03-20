926de79ea53f61717e5733c0855809a405e57c9f
immanuelcherui1 <mmanuelcherui1@gmail.com>
2024-03-21 01:17:43 +0300

    added get_all method on tenants

--- lib/models/tenant_profile.py
+++ lib/models/tenant_profile.py
@@ -10,14 +10,14 @@ class TenantProfile:
         self.room_cost = room_cost
 
     def save(self):
-        CURSOR.execute("INSERT INTO tenant_profile (tenant_national_id, entry_date, room_no, room_cost) VALUES (?, ?, ?, ?)",
+        CURSOR.execute("INSERT INTO tenant_profiles (tenant_national_id, entry_date, room_no, room_cost) VALUES (?, ?, ?, ?)",
                        (self.tenant_national_id, self.entry_date, self.room_no, self.room_cost))
 
     @classmethod
     def create_table(cls):
         """ Create a new table to persist the attributes of TenantProfile instances """
         CURSOR.execute('''
-            CREATE TABLE IF NOT EXISTS tenant_profile (
+            CREATE TABLE IF NOT EXISTS tenant_profiles (
                 id INTEGER PRIMARY KEY,
                 tenant_national_id INTEGER,
                 entry_date TEXT,
@@ -26,3 +26,13 @@ class TenantProfile:
                 FOREIGN KEY (tenant_national_id) REFERENCES profile(national_id)
             )
         ''')
+
+    @classmethod
+    def get_all(cls):
+        """ Retrieve all tenant profiles from the database """
+        CURSOR.execute("""
+            SELECT tenant_profiles.id, tenant_profiles.entry_date, tenant_profiles.room_no, tenant_profiles.room_cost, profiles.name 
+            FROM tenant_profiles 
+            JOIN profiles ON tenant_profiles.tenant_national_id = profiles.national_id
+        """)
+        return CURSOR.fetchall()
