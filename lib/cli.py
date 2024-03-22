bc9e1ead6a419909869acba5a3a9dc6151534d88
immanuelcherui1 <mmanuelcherui1@gmail.com>
2024-03-22 19:59:27 +0300

    Updated CLI

--- lib/cli.py
+++ lib/cli.py
@@ -15,44 +15,95 @@ from helpers import (
 
 def main():
     while True:
-        menu()
+        main_menu()
         choice = input("> ")
         if choice == "0":
             exit_program()
         elif choice == "1":
-            list_profiles()
+            while choice:
+                print("ALL PROFILES")
+                all_profiles_menu()
+                new_choice = input(">")
+                if new_choice == "0":
+                    choice = ""
+                elif new_choice == "1":
+                    list_profiles()
+                elif new_choice == "2":
+                    update_profile()
+                elif new_choice == "3":
+                    find_person_profile_by_national_id()
+        
         elif choice == "2":
-            list_milk_records()
+            while choice:
+                print("DAIRY FARMERS PROFILE")
+                milk_profile_menu()
+                new_choice = input(">")
+                if new_choice == "0":
+                    choice = ""
+                elif new_choice == "1":
+                    list_milk_records()
+                elif new_choice == "2":
+                    list_specific_dairy_farmers_records()
+                
+                  
         elif choice == "3":
-            list_tea_records()
+            while choice:
+                print("TEA PLUCKERS PROFILE")
+                tea_profile_menu()
+                new_choice = input(">")
+                if new_choice == "0":
+                    choice = ""
+                elif new_choice == "1":
+                    list_tea_records()
+                elif new_choice == "2":
+                    list_specific_pluckers_records()
+                      
         elif choice == "4":
-            list_tenant_records()
-        elif choice == "5":
-            find_person_profile_by_national_id()
-        elif choice == "6":
-            update_profile()
-        elif choice == "7":
-            list_specific_dairy_farmers_records()
-        elif choice == "8":
-            list_specific_pluckers_records()
+            while choice:
+                print("TENANTS PROFILE")
+                tenants_profile_menu()
+                new_choice = input(">")
+                if new_choice == "0":
+                    choice = ""
+                elif new_choice == "1":
+                    list_tenant_records()
+
         else:
             print("Invalid choice")
 
-
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. List of Profiles")
-    print("2. Dairy Farmers Records")
-    print("3. Pluckers Records")
-    print("4. Tenants Records ")
-    print("5. Find Person profile by National ID")
-    print("6. Update Profile")
-    print("7. List by Id - Dairy Farmer's Records")
-    print("8. List by Id - Plucker's Records")
+def main_menu():
+    print("Select an option:")
+    print("0. Exit")
+    print("1. Profiles Menu")
+    print("2. Dairy Farmers Profile Menu")
+    print("3. Tea Pluckers Profile Menu")
+    print("4. Tenants Profile Menu")
     
-   
+def all_profiles_menu():
+    print("Select an option:")
+    print("0. Back to main menu")
+    print("1. List of Profiles")
+    print("2. Update Profile")
+    print("3. Find Person profile by National ID")
+       
+def milk_profile_menu():
+    print("Select an option:")
+    print("0. Back to main menu")
+    print("1. Dairy Farmers Records")
+    print("2. List by Id - Dairy Farmer's Records")
+    
+def tea_profile_menu():
+    print("Select an option:")
+    print("0. Back to main menu")
+    print("1. Pluckers Records")
+    print("2. List by Id - Plucker's Records")
 
+def tenants_profile_menu():
+    print("Select an option:")
+    print("0. Back to main menu")
+    print("1. Tenants Records ")
+    
+    
 
 if __name__ == "__main__":
     main()
